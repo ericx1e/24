30489f87d59a2e48f417acd5c1e6d0f4969ae9b2
ericx1e <ericxie6@gmail.com>
2021-05-03 13:50:18 -0400

    improved drawing app

--- sketch.js
+++ sketch.js
@@ -1,50 +1,50 @@
-let sketch = function(p) {
-    p.setup = function() {
+let sketch = function (p) {
+    p.setup = function () {
         p.createCanvas(500, 500);
-        
+
     };
-    
+
     let balls = [];
-    
-    p.draw = function() {
+
+    p.draw = function () {
         p.colorMode(p.RGB);
         p.background(51);
         for (let i = 0; i < balls.length; i++) {
             const ball = balls[i];
             ball.move();
             ball.show();
-            if(ball.x < -ball.size || ball.x > p.width+ball.size || ball.size <= 0) {
+            if (ball.x < -ball.size || ball.x > p.width + ball.size || ball.size <= 0) {
                 balls.splice(i, 1);
                 i--;
             }
-    
+
         }
-    
+
     };
 
-    p.mouseDragged = function() {
+    p.mouseDragged = function () {
         p.noStroke();
         balls.push(new Ball(p.mouseX, p.mouseY));
     }
 
-    function Ball(x, y) {x
+    function Ball(x, y) {
         this.x = x;
-        this.y= y;
+        this.y = y;
         this.xv = p.random(-5, 5);
         this.yv = 0;
         this.size = 50;
         this.color = p.color(p.random(0, 255), 255, 255);
-        this.move = function() {
-            this.x+=this.xv;
-            this.y+=this.yv;
-            this.yv=this.yv+0.5;
-            if(this.y + this.size/2 > p.height) {
-                this.yv = -this.yv*p.random(0.5, 0.9);
-                this.y+=this.yv;
-                this.size--;
+        this.move = function () {
+            this.x += this.xv;
+            this.y += this.yv;
+            this.yv = this.yv + 0.5;
+            this.size -= 0.2;
+            if (this.y + this.size / 2 > p.height) {
+                this.yv = -this.yv * p.random(0.5, 0.9);
+                this.y += this.yv;
             }
         }
-        this.show = function() {
+        this.show = function () {
             p.colorMode(p.HSB);
             p.fill(this.color);
             p.noStroke();
--- sketch1.js
+++ sketch1.js
@@ -1,22 +1,39 @@
-let sketch1 = function(f) {
+let sketch1 = function (f) {
     let buttons = [];
     let brushColor = f.color(0);
+    let colors = [f.color(0), f.color(51), f.color(240), f.color(255, 100, 100), f.color(255, 150, 100), f.color(255, 255, 100), f.color(150, 255, 100), f.color(100, 255, 255), f.color(100, 100, 255), f.color(150, 100, 255)];
 
-    f.setup = function() {
+    let rSlider;
+    let gSlider;
+    let bSlider;
+    let clearButton;
+    let weightSlider;
+    let canvas;
+    let weight = 5;
+
+    f.setup = function () {
         canvas = f.createCanvas(600, 700);
         canvas.position(8, 650);
         f.background(51);
-        f.push();
-        
-        buttons.push(new ColorButton(25, 25, f.color(0, 0, 0)));
-        buttons.push(new ColorButton(100, 25, f.color(255, 100, 100)));
-        buttons.push(new ColorButton(175, 25, f.color(255, 150, 100)));
-        buttons.push(new ColorButton(250, 25, f.color(255, 100, 150)));
-        f.pop();
+
+        for (let i = 0; i < colors.length; i++) {
+            let x = i * 25;
+            let y = 0;
+            let w = 25;
+            let h = 100;
+            buttons.push(new Button(x, y, w, h, colors[i]));
+        }
+
+        rSlider = f.createSlider(0, 255, 0);
+        gSlider = f.createSlider(0, 255, 0);
+        bSlider = f.createSlider(0, 255, 0);
+        weightSlider = f.createSlider(1, 20, 5, 0.5);
+        clearButton = f.createButton('clear');
+        clearButton.mousePressed(function () { f.background(51) });
     };
 
-    
-    f.draw = function() {
+
+    f.draw = function () {
         f.fill(255);
         f.noStroke();
         f.rect(0, 0, f.width, 100);
@@ -24,49 +41,50 @@ let sketch1 = function(f) {
             button.show();
         });
 
-        if(f.mouseIsPressed) {
-            prevMouse.x = f.mouseX;
-            prevMouse.y = f.mouseY;
-        }
+        prevMouse.x = f.mouseX;
+        prevMouse.y = f.mouseY;
+
+        weight = weightSlider.value();
     };
 
-    prevMouse = {x: -1, y: -1};
+    prevMouse = { x: -1, y: -1 };
 
-    f.mouseDragged = function() {
-        if(f.mouseX < 0 || f.mouseX > f.width || f.mouseY < 100 || f.mouseY > f.height) {
+    f.mouseDragged = function () {
+        if (f.mouseX < 0 || f.mouseX > f.width || f.mouseY < 100 || f.mouseY > f.height) {
             return;
         }
         // f.noStroke();
         f.stroke(brushColor);
-        f.strokeWeight(5);
+        f.strokeWeight(weight);
         f.strokeCap(f.ROUND);
         f.line(prevMouse.x, prevMouse.y, f.mouseX, f.mouseY);
     }
 
-    f.mousePressed = function() {
+    f.mousePressed = function () {
         buttons.forEach(button => {
-            if(button.touchingMouse()) {
+            if (button.touchingMouse()) {
                 brushColor = button.c;
             }
         });
     }
 
-    function ColorButton(x, y, c) {
+    function Button(x, y, w, h, c) {
         this.x = x;
         this.y = y;
+        this.w = w;
+        this.h = h;
         this.c = c;
-        this.size = 50;
 
-        this.show = function() {
+        this.show = function () {
             f.fill(c);
             // f.strokeWeight(5);
             // f.stroke(100);
             f.noStroke();
-            f.rect(this.x, this.y, this.size, this.size);
+            f.rect(this.x, this.y, this.w, this.h);
         }
 
-        this.touchingMouse = function() {
-            return f.mouseX < this.x+this.size && f.mouseX > x && f.mouseY < this.y+this.size && f.mouseY > this.y;
+        this.touchingMouse = function () {
+            return f.mouseX < this.x + this.w && f.mouseX > x && f.mouseY < this.y + this.h && f.mouseY > this.y;
         }
     }
 }
